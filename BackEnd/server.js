0f0a25824943c485c26411304df8f39b01cdfd74
Gmarc <126301794+Gmarc27@users.noreply.github.com>
2024-04-17 20:31:04 +0800

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -55,7 +55,7 @@ app.post('/SignupScreen', (req, res) => {
     });
 });
 
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
